993dab82b58f9b39aa173589aeaea278844dbbb2
Sebastian Jadczak <sebastian-jadczak@wp.pl>
2019-10-09 22:07:35 +0200

    add First content with image

--- src/components/Content/FirstContent.js
+++ src/components/Content/FirstContent.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+
+
 const FirstContent = () => {
     return (
 
-        <p></p>
+        <>
+            <div className="image">
+
+            </div>
+            <div className="description">
+                <p>Sprawdź nasz najlepszy hotel!<br /> 200 Pokoi, 8 Saun, 4 Baseny i Stadnina Koni</p>
+                <div className="button">  <button>Zobacz!</button></div>
+            </div>
+
+        </>
     )
 }
 export default FirstContent;
